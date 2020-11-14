a9db79f7ce46c4b1df793f1ac8928f7dab6ada6b
Abed Alhaq <mohammad.abed.alhaq@intel.com>
2020-11-14 14:10:47 +0200

    Data coupling

--- Samples/Program.cs
+++ Samples/Program.cs
@@ -7,13 +7,17 @@ namespace Samples
         static void Main(string[] args)
         {
             Console.WriteLine("Assingment 3");
-            IShape[] ShapeList = new IShape[3] { new Circle(), new Square()};
+            IShape[] ShapeList = new IShape[2] { new Circle(), new Square()};
             foreach (var shape in ShapeList)
             {
                 shape.Draw();
                 shape.Move(3, 3);
                 shape.Draw();
             }
+
+            DBConnection connection = new DBConnection;
+            connection.Username = "user1";
+                connection.Password = "pass1";
         }
     }
 }
