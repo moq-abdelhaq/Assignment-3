93e22556c2037b1058efca14170098859a879d9c
Abed Alhaq <mohammad.abed.alhaq@intel.com>
2020-11-14 14:24:40 +0200

    DatabaseService update

--- Samples/DatabaseService.cs
+++ Samples/DatabaseService.cs
@@ -6,5 +6,16 @@ namespace Samples
 {
     class DatabaseService
     {
+        //This is a class supposed to handle query and connection to SQLite database 
+        public void Connect(DBConnection connection) {
+            //This is an example of data coupling where a DBconnection is needed to be passed 
+            //for this function in order to further create the connection to Database
+        }
+
+        public void Query(String query)
+        {
+            //Query Execution
+        }
+
     }
 }
