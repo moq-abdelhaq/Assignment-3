f4fe5d0064fbe2e81e54f40cebb0abe80008297b
Abed Alhaq <mohammad.abed.alhaq@intel.com>
2020-11-14 14:20:13 +0200

    stamp coupling

--- Samples/DBConnection.cs
+++ Samples/DBConnection.cs
@@ -5,6 +5,14 @@ using System.Text;
 namespace Samples
 {
     class DBConnection
-    {
+    { //DB connection class
+        public string Username { get; set; } = "";
+        public string Password { get; set; } = "";
+
+        //Low Cohesion to include query in a DBconnection class 
+        //passing this to DatabaseService also result into Stamp Coupling, since Database service 
+        //dont relay on Query for creating a connection 
+        public string Query { get; set; }
+
     }
 }
